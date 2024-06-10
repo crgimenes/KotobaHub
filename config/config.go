7cb6d6f125e1532c06f1452081ed8881473b4f68
crgimenes <crg@crg.eti.br>
2024-06-10 07:51:35 -0300

    add IsTTY

--- config/config.go
+++ config/config.go
@@ -1,11 +1,17 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+
+	"golang.org/x/term"
+)
 
 type Config struct {
 	ListemAddress string // Address to listen on
 	DBPath        string // Path to the database file
 	Debug         bool   // Enable debug mode
+	IsTTY         bool   // Is a TTY
 }
 
 var (
@@ -14,6 +20,10 @@ var (
 
 func Load() {
 	CFG.DBPath = "file:data.db?mode=rwc&_journal_mode=WAL&_busy_timeout=10000`"
+
+	if term.IsTerminal(int(os.Stdout.Fd())) {
+		CFG.IsTTY = true
+	}
 
 	flag.StringVar(&CFG.DBPath, "db", CFG.DBPath, "Path to the database file")
 	flag.BoolVar(&CFG.Debug, "debug", false, "Enable debug mode")
